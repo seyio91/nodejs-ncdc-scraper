539cca1a06cb131f1f1cf2eb769b0f759cdb129f
Seyi Obaweya <seyi_obaweya@yahoo.com>
2020-04-20 05:00:17 +0800

    created env variables

--- utils/redis-sub-pub.js
+++ utils/redis-sub-pub.js
@@ -1,5 +1,6 @@
 const asyncRedis = require("async-redis");
-const client = asyncRedis.createClient();
+const { REDIS_HOST, REDIS_PORT } = require('./keys')
+const client = asyncRedis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
 
 const { publish } = require('./event')
 
--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,4 +1,5 @@
 const asyncRedis = require("async-redis");
-const client = asyncRedis.createClient();
+const { REDIS_HOST, REDIS_PORT } = require('./keys')
+const client = asyncRedis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
 
 module.exports = client
