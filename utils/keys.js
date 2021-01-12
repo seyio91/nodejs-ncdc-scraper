1b59cf49f17542e749a64dbf414e5c454bea05ea
seyio91 <seyi_obaweya@yahoo.com>
2021-01-12 21:55:03 +0200

    add redis env

--- utils/keys.js
+++ utils/keys.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 REDIS_HOST = process.env.REDIS_HOST;
 REDIS_PORT = process.env.REDIS_PORT;
+REDIS_PASSWORD = process.env.REDIS_PASSWORD;
 module.exports = {
     REDIS_HOST,
-    REDIS_PORT
+    REDIS_PORT,
+    REDIS_PASSWORD
   };
--- utils/redisClient.js
+++ utils/redisClient.js
@@ -1,5 +1,5 @@
 const asyncRedis = require("async-redis");
-const { REDIS_HOST, REDIS_PORT } = require('./keys')
-const client = asyncRedis.createClient({ host: REDIS_HOST, port: REDIS_PORT });
+const { REDIS_HOST, REDIS_PORT,REDIS_PASSWORD } = require('./keys')
+const client = asyncRedis.createClient({ host: REDIS_HOST, port: REDIS_PORT, password: REDIS_PASSWORD });
 
 module.exports = client
