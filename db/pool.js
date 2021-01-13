35d43536bc6da0dec3145aeb58d8e7ad3084cb02
seyio91 <seyi_obaweya@yahoo.com>
2021-01-13 11:36:54 +0200

    add pg tls connection

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,11 @@
-const { Pool } = require('pg');
+const pg = require('pg');
 const dotenv = require('dotenv');
+
+pg.defaults.ssl = true;
 
 dotenv.config();
 
 const databaseConfig = { connectionString: process.env.DATABASE_URL };
-const pool = new Pool(databaseConfig);
+const pool = new pg.Pool(databaseConfig);
 
 module.exports = pool;
